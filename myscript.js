96af89cb2febf87e4e34f058193f147459f043ff
Armin <armin.hamp@minerva.kgi.edu>
2020-06-03 10:09:20 +0200

    Added a color switch and some logic

--- myscript.js
+++ myscript.js
@@ -1,31 +1,56 @@
 //set up modules
 const gameBoard = (function(){
-    const n = 9;
-    const board= new Array(n).fill(0);
-})();
+    //select game elements
+    const cells=new Array();
+    const instructions=document.querySelector(".instruct");
+    const board=document.querySelector(".board");
+    const side=document.querySelector("input");
+    let red=false;
 
-const displayController = (function(){
+    //figure out which side
     const setUp = function(){
-        //add cells
-        const board=document.querySelector(".board");
+        red=side.checked;
+        console.log(red);
         board.innerHTML="";
-        const cells=new Array();
         for (let i=0;i<9;i++){
             cells[i]=document.createElement('div');
-            cells[i].id='cell'+i;
+            cells[i].id=i;
             cells[i].className='cell';
             cells[i].textContent="1";
             cells[i].style.color="white";
             board.appendChild(cells[i]);
         }
-        //add instructions
-        const instructions=document.querySelector(".instructions");
+    };
+    const step= function(){
+        // instructions.textContent="It's your turn!";
+        cells.forEach(addEventListener('click',function(e){
+            if (red){
+                e.target.style.background="red";
+                e.target.style.color="red";
+            } else {
+                e.target.style.background="blue";
+                e.target.style.color="blue";
+            }
+        }))
     };
     return{
-        setUp
+        setUp,
+        step
     }
 })();
 
-//
-const playButton=document.querySelector(".play");
-playButton.addEventListener('click', () => displayController.setUp());
+const displayController = (function(){
+
+})();
+
+//main
+gameBoard.setUp();
+
+const side=document.querySelector("input");
+side.addEventListener('click',function() {
+    gameBoard.setUp();
+}
+);
+
+
+// gameBoard.step();
